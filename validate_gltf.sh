85008041830c7556276af128ca74540dc0ec7806
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-08-12 20:58:47 -0700

    Format the report.

--- validate_gltf.sh
+++ validate_gltf.sh
@@ -1,8 +1,7 @@
-cat Fox.glb | wc --bytes
-blender --background --python-expr "import bpy; bpy.ops.import_scene.gltf(filepath='`pwd`/Fox.glb', import_pack_images=True, merge_vertices=True, import_shading='NORMALS', bone_heuristic='BLENDER', guess_original_bind_pose=True); bpy.ops.export_scene.gltf(filepath='`pwd`/embedded.gltf', export_format='GLTF_EMBEDDED')"
-cue import json: embedded.gltf -f
-cp report_template.cue report.cue
-cat embedded.cue >> report.cue
-cue vet report.cue schema: check.cue
-cue eval report.cue -e scene_length
-cue eval report.cue -e node_length
+cat Fox.glb | wc --bytes # Get byte count of binary.
+blender --background --python-expr "import bpy; bpy.ops.import_scene.gltf(filepath='`pwd`/Fox.glb', import_pack_images=True, merge_vertices=True, import_shading='NORMALS', bone_heuristic='BLENDER', guess_original_bind_pose=True); bpy.ops.export_scene.gltf(filepath='`pwd`/embedded.gltf', export_format='GLTF_EMBEDDED')" # Export embedded json.
+cue import json: embedded.gltf -f # Convert to cue.
+cp report_template.cue report.cue # Put text at the start of the file.
+cat embedded.cue >> report.cue # Put text at the start of the file.
+cue vet report.cue schema: check.cue # Use checks.
+cue eval report.cue -e {scene: scene_length, node: node_length} # Get report
