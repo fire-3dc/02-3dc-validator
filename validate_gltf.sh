4b6d6cfe86a619dc867b8005febb29035c4aa871
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-08-12 22:52:07 -0700

    Get primitive indices count.

--- validate_gltf.sh
+++ validate_gltf.sh
@@ -8,6 +8,8 @@ cue vet report.cue schema: check.cue # Use checks.
 cue eval report.cue --out text -e buffers[bufferViews[images[0].bufferView].buffer].uri > uri.txt # Where does this go!?
 export FILENAME=$(cue eval report.cue --out text -e buffers[bufferViews[images[0].bufferView].buffer].uri)
 export OFFSET=$(cue eval report.cue -e bufferViews[images[0].bufferView].byteOffset)
-export LENGTH=$(cue eval report.cue -e buffers[bufferViews[images[0].bufferView].buffer].byteLength) 
+export LENGTH=$(cue eval report.cue -e buffers[bufferViews[images[0].bufferView].buffer].byteLength)
 # apt install -y graphicsmagick
 cat $FILENAME | tail -c+$OFFSET | head -c$LENGTH | gm identify -format "{ width: %w, height: %h }" -
+cue eval report.cue -e "{mesh_0_primitive_0_index_count: accessors[meshes[0].primitives[0].indices].count }" # If the type is a triangle mesh it
+cue eval report.cue -e "{mesh_0_primitive_0_triangle_count_if_trimesh: accessors[meshes[0].primitives[0].indices].count / 3 }" # If the type is a triangle mesh
