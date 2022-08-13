59b78ed36e42baa9c98aa76c5b17e22e1f7a5b53
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-08-12 22:32:36 -0700

    Find width and height of one test image.

--- validate_gltf.sh
+++ validate_gltf.sh
@@ -1,7 +1,13 @@
 cat Fox.glb | printf "bytes: %d\n" `wc --bytes` # Get byte count of binary.
-blender --background --python-expr "import bpy; bpy.ops.import_scene.gltf(filepath='`pwd`/Fox.glb', import_pack_images=True, merge_vertices=True, import_shading='NORMALS', bone_heuristic='BLENDER', guess_original_bind_pose=True); bpy.ops.export_scene.gltf(filepath='`pwd`/embedded.gltf', export_format='GLTF_EMBEDDED')" > /dev/null 2>&1  # Export embedded json.
+./gltfpack -i `pwd`/Fox.glb -o `pwd`/embedded.gltf -te -kn -km -ke -noq
 cue import json: embedded.gltf -f # Convert to cue.
 cp report_template.cue report.cue # Put text at the start of the file.
 cat embedded.cue >> report.cue # Put text at the start of the file.
 cue eval report.cue --out json -e "{scenes: scene_count, nodes: node_count, materials: material_count, images: image_count}" # Get report
 cue vet report.cue schema: check.cue # Use checks.
+cue eval report.cue --out text -e buffers[bufferViews[images[0].bufferView].buffer].uri > uri.txt # Where does this go!?
+export FILENAME=$(cue eval report.cue --out text -e buffers[bufferViews[images[0].bufferView].buffer].uri)
+export OFFSET=$(cue eval report.cue -e bufferViews[images[0].bufferView].byteOffset)
+export LENGTH=$(cue eval report.cue -e buffers[bufferViews[images[0].bufferView].buffer].byteLength) 
+# apt install -y graphicsmagick
+cat $FILENAME | tail -c+$OFFSET | head -c$LENGTH | gm identify -format "{ width: %w, height: %h }" -
